f259eb71d9bd69c4ea313de00fc7744311d4ad1e
Sandeep Perumalla <sperumalla@salesforce.com>
2022-03-25 14:07:48 +0530

    General Coding

--- src/main/java/com/redis/testing/RangeScanTester.java
+++ src/main/java/com/redis/testing/RangeScanTester.java
@@ -23,17 +23,15 @@ public class RangeScanTester {
     }
 
     private static void redisZRangeTest() {
-        System.out.println("-----------------------");
-        System.out.println("REDIS OPS");
-
-        Jedis jedis = new Jedis(URI.create("redis://localhost:6379"));
+        print("-----------------------");
+        print("REDIS OPS");
         jedis.del("set1");
         List<String> values = setupData();
-        System.out.println("REDIS INSERT OP");
+        print("REDIS INSERT OP");
         insertDataIntoRedisSortedSet(values);
-        System.out.println("REDIS READ OPS");
+        print("REDIS READ OPS");
         readDataFromSortedSet();
-        System.out.println("-----------------------");
+        print("-----------------------");
     }
 
     private static void insertDataIntoRedisSortedSet(List<String> values) {
@@ -41,13 +39,13 @@ public class RangeScanTester {
         int count = 0;
         int batchSize = 10_000;
         long start = System.nanoTime();
-        for (int i = 0; i < values.size(); i++) {
+        for (String value : values) {
             if (count == batchSize) {
                 addValuesToSet(jedis, redisSortedSetMap);
                 redisSortedSetMap.clear();
                 count = 0;
             }
-            redisSortedSetMap.put(values.get(i), 0d);
+            redisSortedSetMap.put(value, 0d);
             count++;
         }
         addValuesToSet(jedis, redisSortedSetMap);
@@ -64,18 +62,17 @@ public class RangeScanTester {
             String eR = sanitizeDay(dateTime.getDayOfMonth()) + "/" + dateTime.getMonth().getValue() + "/" + dateTime.getYear();
             dateTime = dateTime.minusDays(1);
             String sR = sanitizeDay(dateTime.getDayOfMonth()) + "/" + dateTime.getMonth().getValue() + "/" + dateTime.getYear();
-            System.out.println("Range : [" + sR + " - " + eR + "]");
+            print("Range : [" + sR + " - " + eR + "]");
             Set<String> result = jedis.zrangeByLex("set1", "[" + sR, "[" + eR);
             long time = System.nanoTime() - start;
             printTime(time);
             times.add(time);
-            System.out.println("Result Size : " + result.size());
+            print("Result Size : " + result.size());
         }
 
         IntSummaryStatistics statistics = times.stream().mapToInt(Long::intValue).summaryStatistics();
-        System.out.println("Average Stats");
+        print("Average Stats");
         printTime((long) statistics.getAverage());
-
     }
 
     private static void addValuesToSet(Jedis jedis, Map<String, Double> redisSortedSetMap) {
@@ -83,16 +80,16 @@ public class RangeScanTester {
     }
 
     private static void testNavigationSet() {
-        System.out.println("-----------------------");
-        System.out.println("NAVSET OPS");
+        print("-----------------------");
+        print("NAVSET OPS");
         List<String> values = setupData();
-        System.out.println("NAVSET INSERT OP");
+        print("NAVSET INSERT OP");
         long start = System.nanoTime();
         NavigableSet<String> ns = new TreeSet<>(values);
         printTime(System.nanoTime() - start);
-        System.out.println("Size : " + ns.size());
+        print("Size : " + ns.size());
 
-        System.out.println("NAVSET READ OPS");
+        print("NAVSET READ OPS");
         List<Long> times = new ArrayList<>();
         LocalDateTime dateTime = LocalDateTime.now();
         for (int i = 1; i <= 100; i++) {
@@ -100,24 +97,24 @@ public class RangeScanTester {
             String eR = sanitizeDay(dateTime.getDayOfMonth()) + "/" + dateTime.getMonth().getValue() + "/" + dateTime.getYear();
             dateTime = dateTime.minusDays(1);
             String sR = sanitizeDay(dateTime.getDayOfMonth()) + "/" + dateTime.getMonth().getValue() + "/" + dateTime.getYear();
-            System.out.println("Range : [" + sR + " - " + eR + "]");
+            print("Range : [" + sR + " - " + eR + "]");
             NavigableSet<String> result = new TreeSet<>();
             try {
                 result = ns.subSet(sR, true, eR, true);
             } catch (Exception e) {
-
+                // TODO
             }
             long time = System.nanoTime() - start;
             printTime(time);
             times.add(time);
-            System.out.println("Result Size : " + result.size());
+            print("Result Size : " + result.size());
         }
 
         IntSummaryStatistics statistics = times.stream().mapToInt(Long::intValue).summaryStatistics();
-        System.out.println("Average Stats");
+        print("Average Stats");
         printTime((long) statistics.getAverage());
 
-        System.out.println("-----------------------");
+        print("-----------------------");
 
     }
 
@@ -143,7 +140,7 @@ public class RangeScanTester {
     }
 
     private static void printTime(long timeInNs) {
-        System.out.println("Time: " + TimeUnit.NANOSECONDS.toMillis(timeInNs) + " ms," + TimeUnit.NANOSECONDS.toMicros(timeInNs) + "us");
+        print("Time: " + TimeUnit.NANOSECONDS.toMillis(timeInNs) + " ms," + TimeUnit.NANOSECONDS.toMicros(timeInNs) + "us");
     }
 
     private static List<String> getDates() {
@@ -159,6 +156,10 @@ public class RangeScanTester {
     private static String sanitizeDay(int d) {
         String day = String.valueOf(d);
         return day.length() == 1 ? "0" + day : day;
+    }
+
+    private static void print(String message) {
+        System.out.println(message);
     }
 
 
